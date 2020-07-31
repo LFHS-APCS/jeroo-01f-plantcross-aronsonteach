1165f209e73eb380ace7eca144930b2c51c16756
Steve Aronson <saronson@lfschools.net>
2020-07-31 15:24:04 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * Plant flowers around the cross as described in the Readme.md
+ * Use left and right arrow keys to see it when running main()
+ */
+public class Main implements Directions {
+
+    /**
+     * Do NOT edit this.  Put your code inside the runJerooCode method below.
+     */
+    public static void main(String[] args) {
+        Map.getInstance().loadMap("maps/plantCross.jev");
+        new JerooGUI();
+        runJerooCode();
+        Map.getInstance().printMap();
+    }
+
+    /**
+     * Put your main Jeroo code here.
+     */
+    public static void runJerooCode() {
+       // Write code here to make a letter here
+       Jeroo kim = new Jeroo(0, 0, EAST, 100);
+       kim.plantCross();
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+Write the method ```plantCross()``` in Jeroo.java that will plant flowers around the cross as shown in the pictures below.
+
+IMPORTANT:  You can only use:
+* ```hop()```
+* ```plant()```
+* ```turn(LEFT)```
+* ```turn(RIGHT)```
+
+You should be using helper methods.  Do NOT put all the code inside of plantCross()
+
+![plantCrossBefore](https://user-images.githubusercontent.com/28961298/89072869-02b46a80-d33f-11ea-92df-2b2939d880cb.jpg)
+
+![plantCrossAfter](https://user-images.githubusercontent.com/28961298/89072890-0a740f00-d33f-11ea-982c-1b078c11e207.jpg)
+
+
+
+
+
